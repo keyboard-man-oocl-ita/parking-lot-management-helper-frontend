361178c7a5e7d9a0bde796af414c176c33c46b21
Jimmy Ou <jimmy.ou@oocl.com>
2019-07-30 20:51:55 +0800

    add orders module in store

---
+++ src/api/order.js
@@ -0,0 +1,9 @@
+import request from '@/utils/request'
+
+export function fetchOrder(params) {
+  return request({
+    url: '/orders',
+    method: 'get',
+    params: params
+  })
+}
---
+++ src/store/modules/order.js
@@ -0,0 +1,40 @@
+import { fetchOrder } from '@/api/order'
+
+const state = {
+  orders: [],
+  total: 0,
+  currentPage: 0
+}
+
+const mutations = {
+  LOAD_ORDERS: (state, orders) => {
+    state.orders = orders
+    state.total = orders.length
+  },
+  SET_CURRENT_PAGE: (state, index) => {
+    state.currentPage = index
+  },
+  SET_TOTAL: (state, total) => {
+    state.total = total
+  }
+}
+
+const actions = {
+  async loadOrders({ commit }, params) {
+    const result = await fetchOrder(params)
+    commit('LOAD_ORDERS', result)
+  },
+  setCurrentPage({ commit }, index) {
+    commit('SET_CURRENT_PAGE', index)
+  },
+  setTotal({ commit }, total) {
+    commit('SET_TOTAL', total)
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
