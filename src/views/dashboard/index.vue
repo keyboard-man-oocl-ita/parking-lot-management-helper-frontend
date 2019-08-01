0155b5eb1f2912dbbd9173cd01348f88b7592202
Jimmy Ou <jimmy.ou@oocl.com>
2019-08-01 12:25:34 +0800

    [jimmy] refactor dashboard: remove unused module

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,31 +1,25 @@
 <template>
   <div class="dashboard-container">
-    <component :is="currentRole" />
+    <adminDashboard />
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import adminDashboard from './admin'
-import editorDashboard from './editor'
 
 export default {
   name: 'Dashboard',
-  components: { adminDashboard, editorDashboard },
+  components: { adminDashboard },
   data() {
     return {
-      currentRole: 'adminDashboard'
+      showDashboard: true
     }
   },
   computed: {
     ...mapGetters([
       'roles'
     ])
-  },
-  created() {
-    if (!this.roles.includes('admin')) {
-      this.currentRole = 'editorDashboard'
-    }
   }
 }
 </script>
